3483a979a8e77e8e16612a822376b44d0c36cfa8
wwallyson <wallysonsillva436@gmail.com>
2024-08-13 21:17:32 -0300

    Novas alterações

--- src/componentes/CardInfo/index.jsx
+++ src/componentes/CardInfo/index.jsx
@@ -1,44 +1,50 @@
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
+import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import PersonIcon from '@mui/icons-material/Person';
 
 const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    
-  </Box>
+    <Box
+        component="span"
+        sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+    >
+
+    </Box>
 );
 
 export default function CardInfo() {
-  return (
-    <Card sx={{ minWidth: 275, maxWidth: 300 }}>
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.primary" gutterBottom>
-          Informações Pessoais
-        </Typography>
-        <Typography variant="h6" component="div">
-          Joana Magalhães Souza
-        </Typography>
-        <Typography sx={{ mb: 1.0 }} color="text.secondary">
-          28/08/1990
-        </Typography>
-        <Typography sx={{ mb: 1.0 }} color="text.secondary">
-          São Paulo - SP
-        </Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-  );
+    return (
+        <Card sx={{ minWidth: 275, maxWidth: 300 }}>
+            <CardContent>
+                <Typography sx={{ fontSize: 16, fontWeight: 'bold', textAlign: 'left', color: "#339CFF", display: 'flex', marginBottom: 1 }} color="text.primary" gutterBottom>
+                    <PersonIcon />     Informações Pessoais
+                </Typography>
+                <Typography sx={{ fontSize: 14, fontWeight: 'bold', textAlign: 'left' }} variant="h6" component="div">
+                    Joana Magalhães Souza
+                </Typography>
+                <Typography sx={{ fontSize: 14, textAlign: 'left' }} color="text.secondary">
+                    28/08/1990
+                </Typography>
+                <Typography sx={{ fontSize: 14, textAlign: 'left' }} color="text.secondary">
+                    São Paulo - SP
+                </Typography>
+                <Typography variant="body2" sx={{ marginTop: 3 }} >
+                    <Button variant="contained" sx={{
+                        backgroundColor: "#0B3B60",
+                        color: "white",
+                        textTransform: 'none',
+                        '&:hover': {
+                            backgroundColor: "#0B3B60",
+                        }
+                    }}>
+                        Ver mais
+                    </Button>
+                </Typography>
+            </CardContent>
+
+        </Card>
+    );
 }
