d1282aaa41cf8604335646dd1349d713959797cc
willianjonhy05 <willianjohny05@gmail.com>
2024-08-14 17:16:47 -0300

    Reload My Tabs

--- src/componentes/MyTabs/index.jsx
+++ src/componentes/MyTabs/index.jsx
@@ -9,44 +9,64 @@ import PersonIcon from '@mui/icons-material/Person';
 export default function MyTabs() {
 
   return (
-    <Tabs sx={{ justifyContent: 'center', backgroundColor: '#0B3B60' }} aria-label="icon label tabs example">
+    <Tabs sx={{ justifyContent: 'center', backgroundColor: '#0B3B60',  width: 360, height: 76,  }} aria-label="icon label tabs example">
       <Tab 
         sx={{ 
           color: 'white', 
           alignItems: 'center',
           justifyContent: 'center',
+          position: 'relative',
           flexDirection: 'column',
+          padding: '16px 42px',
+          textTransform: 'none',
+          width: 56,
+          height: 44, 
         }} 
         icon={<HomeIcon />} 
         label="Início" 
       />
       <Tab 
         sx={{ 
-          color: 'white', 
-          alignItems: 'center',
-          justifyContent: 'center',
-          flexDirection: 'column',
-        }} 
+            color: 'white', 
+            alignItems: 'center',
+            justifyContent: 'center',
+            position: 'relative',
+            flexDirection: 'column',
+            padding: '16px 42px',
+            textTransform: 'none',
+            width: 56,
+            height: 44, 
+          }} 
         icon={<CalendarMonthIcon />} 
         label="Consultas" 
       />
       <Tab 
         sx={{ 
-          color: 'white', 
-          alignItems: 'center',
-          justifyContent: 'center',
-          flexDirection: 'column',
-        }} 
+            color: 'white', 
+            alignItems: 'center',
+            justifyContent: 'center',
+            position: 'relative',
+            flexDirection: 'column',
+            padding: '16px 42px',
+            textTransform: 'none',
+            width: 56,
+            height: 44, 
+          }} 
         icon={<SearchIcon />} 
         label="Explorar" 
       />
       <Tab 
         sx={{ 
-          color: 'white', 
-          alignItems: 'center',
-          justifyContent: 'center',
-          flexDirection: 'column',
-        }} 
+            color: 'white', 
+            alignItems: 'center',
+            justifyContent: 'center',
+            position: 'relative',
+            flexDirection: 'column',
+            padding: '16px 42px',
+            textTransform: 'none',
+            width: 56,
+            height: 44, 
+          }} 
         icon={<PersonIcon />} 
         label="Perfil" 
       />
